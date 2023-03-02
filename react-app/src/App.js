e69ee40a05ef645a42a97e752d31eaa74ca640d4
Johnny San <johnny.san@gmail.com>
2023-03-02 10:11:43 -0800

    login form shown

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
 import { authenticate } from "./store/session";
 import Modal from "./components/modal/modal";
+import LoginForm from "./components/auth/LoginForm";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
 
   useEffect(() => {
     (async () => {
@@ -35,9 +37,16 @@ function App() {
           <User />
         </ProtectedRoute>
         <Route path="/" exact={true}>
-          <div className="container-fluid m-2 mx-auto text-warning">
-            <h1>Your Pregnancy Tracker</h1>
-          </div>
+          {user ? (
+            <div className="container-fluid m-2 mx-auto text-warning">
+              <h1>Your Pregnancy Tracker</h1>
+            </div>
+          ) : (
+            <div className="container text-warning justify-content-around p-2">
+              <h1>Please Login</h1>
+              <LoginForm />
+            </div>
+          )}
         </Route>
       </Switch>
     </BrowserRouter>
